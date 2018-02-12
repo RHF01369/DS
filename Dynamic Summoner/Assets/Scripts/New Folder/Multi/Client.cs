f2aa3f34dec2579d841242cc57d976b4fb1e2b05
CentumBao <CentumBao@ds.com>
2018-02-13 07:27:38 +0900

    Made two receiveBuffers

--- Dynamic Summoner/Assets/Scripts/New Folder/Multi/Client.cs
+++ Dynamic Summoner/Assets/Scripts/New Folder/Multi/Client.cs
@@ -19,25 +19,32 @@ public class Client
 
     private Socket receiveSocket;
 
-    private byte[] receiveBuffer;
+    private byte[][] receiveBuffer;
     private byte[] sendBuffer;
 
     private int playerNumber;
 
-    private Dictionary<PacketType, Action> packetTypeToAction;
-    private SocketAsyncEventArgs socketAsyncEventArgs;
+    private Dictionary<PacketType, Action<byte[]>> packetTypeToAction;
+    private SocketAsyncEventArgs socketAsyncEventArgs0;
+    private SocketAsyncEventArgs socketAsyncEventArgs1;
 
     public Client()
     {
         receiveSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        receiveBuffer = new byte[1024];
+        receiveBuffer = new byte[2][];
+        receiveBuffer[0] = new byte[1024];
+        receiveBuffer[1] = new byte[1024];
         sendBuffer = new byte[1024];
 
-        socketAsyncEventArgs = new SocketAsyncEventArgs();
-        socketAsyncEventArgs.SetBuffer(receiveBuffer, 0, receiveBuffer.Length);
-        socketAsyncEventArgs.Completed += ReceivePacket;
+        socketAsyncEventArgs0 = new SocketAsyncEventArgs();
+        socketAsyncEventArgs0.SetBuffer(receiveBuffer[0], 0, receiveBuffer[0].Length);
+        socketAsyncEventArgs0.Completed += ReceivePacket0;
 
-        packetTypeToAction = new Dictionary<PacketType, Action>()
+        socketAsyncEventArgs1 = new SocketAsyncEventArgs();
+        socketAsyncEventArgs1.SetBuffer(receiveBuffer[1], 0, receiveBuffer[1].Length);
+        socketAsyncEventArgs1.Completed += ReceivePacket1;
+
+        packetTypeToAction = new Dictionary<PacketType, Action<byte[]>>()
         {
             {PacketType.PlayerNumber,       ReceivePlayerNumber},
             {PacketType.Attack,             ReceiveAttack},
@@ -49,11 +56,18 @@ public class Client
         };
     }
 
-    private void ReceivePacket(object sender, SocketAsyncEventArgs e)
+    private void ReceivePacket0(object sender, SocketAsyncEventArgs e)
     {
         //throw new NotImplementedException();
-        receiveSocket.ReceiveAsync(e);
-        ClassifyReceivedPacket();
+        receiveSocket.ReceiveAsync(socketAsyncEventArgs1);
+        ClassifyReceivedPacket(e.Buffer);
+    }
+
+    private void ReceivePacket1(object sender, SocketAsyncEventArgs e)
+    {
+        //throw new NotImplementedException();
+        receiveSocket.ReceiveAsync(socketAsyncEventArgs0);
+        ClassifyReceivedPacket(e.Buffer);
     }
 
     public void Start()
@@ -65,41 +79,41 @@ public class Client
         Setting.GameState = GameState.Battle;
         Setting.GameMode = GameMode.Multi;
 
-        receiveSocket.ReceiveAsync(socketAsyncEventArgs);
+        receiveSocket.ReceiveAsync(socketAsyncEventArgs0);
 
         MultiBattle.Initialize(this);
         MultiBattle.instance.StartMakingEnemyDeck();
     }
 
-    private void ClassifyReceivedPacket()
+    private void ClassifyReceivedPacket(byte[] buffer)
     {
-        PacketType packetType = (PacketType)ByteConverter.ToInt(receiveBuffer, 0);
+        PacketType packetType = (PacketType)ByteConverter.ToInt(buffer, 0);
         Debug.Log(LogType.Trace, "ClassifyReceivedPacket {0}", packetType);
 
-        packetTypeToAction[packetType]();
+        packetTypeToAction[packetType](buffer);
     }
 
-    private void ReceivePlayerNumber()
+    private void ReceivePlayerNumber(byte[] buffer)
     {
-        playerNumber = ByteConverter.ToInt(receiveBuffer, 4);
+        playerNumber = ByteConverter.ToInt(buffer, 4);
     }
 
-    private void ReceiveDeckDataRequest()
+    private void ReceiveDeckDataRequest(byte[] buffer)
     {
         Debug.Log(LogType.Trace, "ReceiveDeckDataRequest");
 
         SendDeckData();
     }
 
-    private void ReceiveEnemyDeckData()
+    private void ReceiveEnemyDeckData(byte[] buffer)
     {
         Debug.Log(LogType.Trace, "ReceiveEnemyDeckData");
 
-        MultiBattleDataManager.enemyDeckData.enemyLevel = ByteConverter.ToInt(receiveBuffer, 8);
+        MultiBattleDataManager.enemyDeckData.enemyLevel = ByteConverter.ToInt(buffer, 8);
         for(int index = 0; index < Setting.DeckCount; index++)
         {
-            MultiBattleDataManager.enemyDeckData.summonNumber[index] = ByteConverter.ToInt(receiveBuffer, 12 + index * 8);
-            MultiBattleDataManager.enemyDeckData.summonLevel[index] = ByteConverter.ToInt(receiveBuffer, 16 + index * 8);
+            MultiBattleDataManager.enemyDeckData.summonNumber[index] = ByteConverter.ToInt(buffer, 12 + index * 8);
+            MultiBattleDataManager.enemyDeckData.summonLevel[index] = ByteConverter.ToInt(buffer, 16 + index * 8);
         }
 
         MultiBattleDataManager.enemyDeckData.isReceived = true;
@@ -107,14 +121,14 @@ public class Client
         SendReadyCompletePacket();
     }
 
-    private void ReceiveBattleStart()
+    private void ReceiveBattleStart(byte[] buffer)
     {
         Debug.Log(LogType.Trace, "ReceiveBattleStart");
     }
 
-    private void ReceiveAttack()
+    private void ReceiveAttack(byte[] buffer)
     {
-        int executionOrder = ByteConverter.ToInt(receiveBuffer, 8);
+        int executionOrder = ByteConverter.ToInt(buffer, 8);
 
         if (executionOrder < MultiBattleDataManager.executionDataOrder)
             return;
@@ -132,17 +146,17 @@ public class Client
             MultiBattleDataManager.AddOutOfSequenceData(executionData);
     }
 
-    private void ReceiveSummon()
+    private void ReceiveSummon(byte[] buffer)
     {
-        int executionOrder = ByteConverter.ToInt(receiveBuffer, 8);
-        int playerNumber = ByteConverter.ToInt(receiveBuffer, 12);
+        int executionOrder = ByteConverter.ToInt(buffer, 8);
+        int playerNumber = ByteConverter.ToInt(buffer, 12);
 
         if (executionOrder < MultiBattleDataManager.executionDataOrder)
             return;
 
-        int summonDeckNumber = ByteConverter.ToInt(receiveBuffer, 16);
-        float xPos = ByteConverter.ToFloat(receiveBuffer, 20);
-        float yPos = ByteConverter.ToFloat(receiveBuffer, 24);
+        int summonDeckNumber = ByteConverter.ToInt(buffer, 16);
+        float xPos = ByteConverter.ToFloat(buffer, 20);
+        float yPos = ByteConverter.ToFloat(buffer, 24);
 
         ExecutionData executionData = new ExecutionData()
         {
@@ -161,7 +175,7 @@ public class Client
             MultiBattleDataManager.AddOutOfSequenceData(executionData);
     }
 
-    private void ReceiveSkill()
+    private void ReceiveSkill(byte[] buffer)
     {
 
     }
@@ -172,16 +186,15 @@ public class Client
         Debug.Log(LogType.Trace, "SendClientData");
 
         int packetSize = 0;
-        ByteConverter.FromString(nickName, receiveBuffer, ref packetSize);
+        ByteConverter.FromString(nickName, sendBuffer, ref packetSize);
         packetSize = 10;
-        ByteConverter.FromInt(rankScore, receiveBuffer, ref packetSize);
-        receiveSocket.Send(receiveBuffer, packetSize, SocketFlags.None);
+        ByteConverter.FromInt(rankScore, sendBuffer, ref packetSize);
+        receiveSocket.Send(sendBuffer, packetSize, SocketFlags.None);
     }
 
     public void SendDeckData()
     {
         int packetSize = 0;
-
         ByteConverter.FromInt((int)PacketType.EnemyDeckData, sendBuffer, ref packetSize);
         ByteConverter.FromInt(0, sendBuffer, ref packetSize);
         ByteConverter.FromInt(UserData.Level, sendBuffer, ref packetSize);
